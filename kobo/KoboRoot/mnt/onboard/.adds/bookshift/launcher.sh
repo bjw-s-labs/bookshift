97aafc0fa7b956237a8d6c3e7c7957eb413c0703
Bernd Schorgers <me@bjw-s.dev>
2025-08-31 20:40:24 +0200

    fix(kobo)!: Move the install location

--- kobo/KoboRoot/mnt/onboard/.adds/bookshift/launcher.sh
+++ kobo/KoboRoot/mnt/onboard/.adds/bookshift/launcher.sh
@@ -8,7 +8,7 @@ if [ ! -f "$BOOKSHIFT_CONFIG_FILE" ]; then
   echo "BookShift config file not found, creating default config..."
   logger -t "BookShift" -p daemon.warning "Launcher: BookShift config file not found, creating default config..."
   mkdir -p /mnt/onboard/.adds/bookshift
-  cp /usr/local/bookshift/config.yaml.dist $BOOKSHIFT_CONFIG_FILE
+  cp /mnt/onboard/.adds/bookshift/config.yaml.dist $BOOKSHIFT_CONFIG_FILE
   logger -t "BookShift" -p daemon.warning "Launcher: BookShift config file created"
 fi
 
@@ -16,12 +16,13 @@ UNINSTALL=/mnt/onboard/.adds/bookshift/UNINSTALL
 if [ -f "$UNINSTALL" ]; then
     echo "$UNINSTALL exists, removing BookShift..."
     logger -t "BookShift" -p daemon.warning "Launcher: BookShift UNINSTALL file located, removing BookShift..."
-    ./usr/local/bookshift/bookshift kobo uninstall
+    /mnt/onboard/.adds/bookshift/bookshift kobo uninstall
     logger -t "BookShift" -p daemon.warning "Launcher: BookShift removed"
 else
     echo "Running BookShift..."
     logger -t "BookShift" -p daemon.warning "Launcher: BookShift binary execution started"
-    /usr/local/bookshift/bookshift run
+    mkdir -p /mnt/onboard/BookshiftLibrary
+    /mnt/onboard/.adds/bookshift/bookshift run
     logger -t "BookShift" -p daemon.warning "Launcher: BookShift binary execution finished"
 fi
 logger -t "BookShift" -p daemon.warning "Launcher: finished"
